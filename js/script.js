76fc6252dae08491dccd6e6df15e2eed7acbb164
Wissembrahem <wbrahem19@gmail.com>
2025-11-21 12:16:16 +0100

    termine

--- js/script.js
+++ js/script.js
@@ -5,18 +5,27 @@ axios
     .then((resp) => {
         const postsArray = resp.data;
         postsArray.forEach((curPost) => {
-            console.log(curPost)
-            myHero.innerHTML += `<div class="col flex justify-content">
+            myHero.innerHTML += `<div class=" col flex justify-content">
             <img class="pin" src="img/pin.svg" >
-                    <div class="card polaroid">
+                    <div class="card polaroid" data-url="${curPost.url}">
                         <img src= "${curPost.url}">
                         <h3 class="card-mension">dedica</h3>
                     </div>
                 </div>`
         })
+      
+        const arrayFoto = document.querySelectorAll(".polaroid");
+        arrayFoto.forEach(function (foto) {
+            foto.addEventListener("click", function () {
+                overlay.classList.remove("d-none")
+                polaroid.innerHTML = `<img src="${this.dataset.url}" alt="Foto">`
+            });
+        });
+
     });
-myHero.forEach((myHeroElem) => {
-    myHeroElem.addEvenListener("click", function () {
-        alert("click");
-    });
+  const overlay = document.querySelector(".overlay");
+        const polaroid = document.querySelector(".center")
+        const polaroids = document.querySelectorAll(".curPost img");
+   overlay.addEventListener("click", function () {
+    overlay.classList.add("d-none");
 });
